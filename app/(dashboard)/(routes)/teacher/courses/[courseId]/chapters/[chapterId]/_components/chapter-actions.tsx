19b14f4fe7fe4fb9cb62056335bd667d0df4be57
Abukhadeejah <arsalanahmed82@hotmail.com>
2024-07-12 06:17:48 +0530

    publish unpublish

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -25,6 +25,26 @@ export const ChapterActions = ({
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
 
+    const onClick = async () => {
+        try {
+            setIsLoading(true);
+
+            if (isPublished) {
+                await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/unpublish`);
+                toast.success("Chapter unpublished successfully");
+            } else {
+                await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/publish`);
+                toast.success("Chapter published successfully");
+            }
+
+            router.refresh();
+        } catch {
+            toast.error("Something went wrong");
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     const onDelete = async () => {
         try{
             setIsLoading(true);
@@ -42,7 +62,7 @@ export const ChapterActions = ({
     return (
         <div className="flex items-center gap-x-2">
               <Button
-                onClick={() => {}}
+                onClick={onClick}
                 disabled={disabled || isLoading}
                 variant="outline"
                 size="sm"
