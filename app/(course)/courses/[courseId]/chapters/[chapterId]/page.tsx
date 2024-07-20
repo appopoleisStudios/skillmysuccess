836f7ddea2fd6ea983cf3463498121aedd1581f5
Abukhadeejah <arsalanahmed82@hotmail.com>
2024-07-20 19:02:34 +0530

    free locked videos player

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,5 +1,9 @@
+import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { VideoPlayer } from "./_components/video-player";
+
 
 const ChapterIdPage = async ({
     params
@@ -11,11 +15,56 @@ const ChapterIdPage = async ({
     if (!userId) {
         return redirect("/");
     }
+
+    const {
+        chapter,
+        course,
+        muxData,
+        nextChapter,
+        userProgress,
+        purchase
+    } = await getChapter({
+        userId: userId,
+        courseId: params.courseId,
+        chapterId: params.chapterId,
+    })
+
+    if (!chapter || !course) {
+        return redirect("/");
+    }
+
+    const isLocked = !chapter.isFree && !purchase;
+    const completeOnEnd = !!purchase && !userProgress?.isCompleted
+
     return ( 
         <div>
-            ChapterIdPage
+            {userProgress?.isCompleted && (
+                <Banner 
+                    variant="success" 
+                    label="You have completed this chapter."
+                />
+            )}
+            {isLocked && (
+                <Banner
+                    variant="warning"
+                    label="You need to purchase this course to watch this chapter."
+                />
+            )}
+            <div className="flex flex-col max-w-4xl mx-auto pb-20">
+                <div className="p-4">
+                    <VideoPlayer 
+                        chapterId={params.chapterId}
+                        title={chapter.title}
+                        courseId={params.courseId}
+                        nextChapterId={nextChapter?.id}
+                        playbackId={muxData?.playbackId!}
+                        isLocked={isLocked}
+                        completeOnEnd={completeOnEnd}
+                    />
+                </div>
+            </div>
         </div>
-     );
+     ); 
 }
  
 export default ChapterIdPage;
