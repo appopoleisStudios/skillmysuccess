dfbed759153c9a955e52e01af11cf9df879b73cc
Abukhadeejah <arsalanahmed82@hotmail.com>
2024-07-20 15:45:33 +0530

    get chapter actions

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,4 +1,16 @@
-const ChapterIdPage = () => {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+    params
+}: {
+    params: { courseId: string; chapterId: string },
+}) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
     return ( 
         <div>
             ChapterIdPage
