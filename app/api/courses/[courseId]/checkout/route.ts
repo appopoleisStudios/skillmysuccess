f6d8ff4cec0c33eeda0c2bd9ea05309ae22383c7
Abukhadeejah <arsalanahmed82@hotmail.com>
2024-07-23 21:49:53 +0530

    half phonepe

---
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -0,0 +1,48 @@
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+import PhonepeGateway from "phonepepg";
+
+export async function POST(
+    req: Request,
+    { params }: { params: { courseId: string; }}
+) {
+    try {
+        const user = await currentUser();
+
+        if (!user || !user.id || !user.emailAddresses?.[0]?.emailAddress) {
+            return new NextResponse("Unauthorized", { status: 400 });
+        }
+
+        const course  = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                isPublished: true,
+            },
+        });
+
+        const purchased = await db.purchase.findUnique({
+            where: {
+                userId_courseId: {
+                    userId: user.id,
+                    courseId: params.courseId,
+                }
+            }
+        })
+
+        if (purchased) {
+            return new NextResponse("Already Purchased", { status: 400 });
+        }
+
+        if (!course) {
+            return new NextResponse("Course Not Found", { status: 404 });
+        }
+
+   
+
+    } catch (error) {
+        console.log("[COURSE_ID_CHECKOUT]", error);
+        return new NextResponse("Internal Server Error", { status: 500})
+    }
+}
