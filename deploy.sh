aaeb684c5ae5fec430e70d5f491ddbd0d191bc3d
Abukhadeejah <arsalanahmed82@hotmail.com>
2024-08-14 08:03:49 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
-ubuntu@vps-38929c8c:~/projects/skillmysuccess/skillmysuccess$ cat deploy.sh
 #!/bin/bash
 
 # Ensure this script is executable
 chmod +x deploy.sh
 
 # Navigate to the project directory
-# cd /home/ubuntu/projects/skillmysuccess/skillmysuccess
+cd /home/ubuntu/projects/skillmysuccess/skillmysuccess
 
 # Stash local changes
 git stash -u
